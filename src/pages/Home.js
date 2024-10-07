6e075b41c6b82813d15ebe07c86c829b4e149e98
vitorcwa <contato.vitorchaves@gmail.com>
2024-10-07 13:56:23 -0300

    Atualização de  versão do node 6

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -65,7 +65,6 @@ export default function Home() {
             linkButton='https://api.whatsapp.com/send?phone=5521986900206&text=Oi,%20eu%20quero%20falar%20com%20um%20consultor!'
          />
          // <HeaderBanner />
-         
          <Vantagens />
          <Experimente />
          <VeiculosDestaque />
