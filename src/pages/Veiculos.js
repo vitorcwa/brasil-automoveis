85de0dc713a246473da35e7264e91871213169b9
Vitor Chaves <98360870+vitorcwa@users.noreply.github.com>
2025-03-11 23:35:07 -0300

    Update Veiculos.js

--- src/pages/Veiculos.js
+++ src/pages/Veiculos.js
@@ -284,7 +284,7 @@ export default function Veiculos() {
                            {filterRecords.map((veiculo, ind)=>(
                               <Grid item xs={12} md={4} key={ind}>
                                  <Box className={classes.box} key={veiculo.Codigo}>
-                                    <Link to={`/veiculo/${veiculo.Codigo}`} className={classes.link}>
+                                    <Link to={`/veiculo/${veiculo.Modelo}/${veiculo.Codigo}`} className={classes.link}>
                                        {
                                           veiculo.FotoPrincipal ?
 
